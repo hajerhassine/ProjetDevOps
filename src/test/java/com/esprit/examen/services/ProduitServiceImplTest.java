61b1c18288c385bf942229f51ce867c1808908c4
lassouedhamza <43931046+lassouedhamza@users.noreply.github.com>
2022-11-03 22:49:26 +0100

    update pom

---
+++ src/test/java/com/esprit/examen/services/ProduitServiceImplTest.java
@@ -0,0 +1,32 @@
+package com.esprit.examen.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProduitServiceImplTest {
+
+    @Test
+    void retrieveAllProduits() {
+    }
+
+    @Test
+    void addProduit() {
+    }
+
+    @Test
+    void deleteProduit() {
+    }
+
+    @Test
+    void updateProduit() {
+    }
+
+    @Test
+    void retrieveProduit() {
+    }
+
+    @Test
+    void assignProduitToStock() {
+    }
+}
